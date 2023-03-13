07bf44c64fbc30c49b7581dd835aaeb0e271f533
cdesio <chiara.desio@bristol.ac.uk>
2023-03-13 15:30:35 +0300

    plotting DSB and SSB by dose and not

--- plot_results_DNA.py
+++ plot_results_DNA.py
@@ -8,34 +8,55 @@ def plot_results(folder, fname_prefix, spacing, nevents, savefig=True, n_div_r=2
     radii = range(n_div_r)
     if spacing:
         if seed:
-            fnames = [os.path.join(
+            fnames_SSB = [os.path.join(
             folder, f"{fname_prefix}_{spacing}um_"+f"{seed}"+f"_{r}.csv") for r in radii]
+            fnames_DSB = [os.path.join(
+            folder, f"{fname_prefix}_{spacing}um_"+f"{seed}"+f"_DSB_{r}.csv") for r in radii]
         else:    
-            fnames = [os.path.join(
+            fnames_SSB = [os.path.join(
                 folder, f"{fname_prefix}_{spacing}um_{r}.csv") for r in radii]
+            fnames_DSB = [os.path.join(
+                folder, f"{fname_prefix}_{spacing}um_DSB_{r}.csv") for r in radii]
     else:
-        fnames = [os.path.join(
+        fnames_SSB = [os.path.join(
             folder, f"{fname_prefix}_{r}.csv") for r in radii]
-    dataset = {}
+        fnames_DSB = [os.path.join(
+            folder, f"{fname_prefix}_DSB_{r}.csv") for r in radii]
+    
+    dataset_SSB = {}
+    dataset_DSB = {}
 
-    for i, f in zip(radii, fnames):
-        to_combine = []
-        to_combine.append(readIN(f))
-        if len(to_combine) != 0:
-            dataset[i] = combine(to_combine)
+
+    for i, fSSB, fDSB in zip(radii, fnames_SSB, fnames_DSB):
+        to_combine_SSB = []
+        to_combine_DSB = []
+        to_combine_SSB.append(readIN(fSSB))
+        to_combine_DSB.append(readIN(fDSB))
+        if len(to_combine_SSB) != 0:
+            dataset_SSB[i] = combine(to_combine_SSB)
+        if len(to_combine_DSB) != 0:
+            dataset_DSB[i] = combine(to_combine_DSB)
     # plot strand breaks vs LET & energy per dose
-    en = []
-    total = []
-    direct = []
-    indirect = []
+    
+    total_SSB = []
+    direct_SSB = []
+    indirect_SSB = []
+
+    dose_SSB_tot = []
+    dose_SSB_direct = []
+    dose_SSB_indirect = []
+
+    total_DSB = []
+    direct_DSB = []
+    indirect_DSB = []
+
+    dose_DSB_tot = []
+    dose_DSB_direct = []
+    dose_DSB_indirect = []
 
     totalErr = []
     directErr = []
     indirectErr = []
-
-    dose_tot = []
-    dose_direct = []
-    dose_indirect = []
 
     radii_out = []
 
@@ -45,28 +66,72 @@ def plot_results(folder, fname_prefix, spacing, nevents, savefig=True, n_div_r=2
     for i in radii:
 
         #en.append(getEnergy(dataset[i]))
-        a, d, n = calcBreaksperDose("TotalSBtotal", (dataset[i]))
-        total.append(a/d/n if d else 0)
-        dose_tot.append(d if d else 0)
-        a,d, n = calcBreaksperDose("TotalSBdirect", (dataset[i]))
-        direct.append(a/d/n if d else 0)
-        dose_direct.append(d if d else 0)
-        a,d, n = calcBreaksperDose("TotalSBindirect", (dataset[i]))
-        indirect.append(a/d/n if d else 0)
-        dose_indirect.append(d if d else 0)
+        nSSB, dose, nGBP = calcBreaksperDose("TotalSBtotal", (dataset_SSB[i]))
+        total_SSB.append(nSSB/nGBP)
+        dose_SSB_tot.append(dose if dose else 0)
+        nSSB, dose, nGBP = calcBreaksperDose("TotalSBdirect", (dataset_SSB[i]))
+        direct_SSB.append(nSSB/nGBP)
+        dose_SSB_direct.append(dose if dose else 0)
+        nSSB, dose, nGBP = calcBreaksperDose("TotalSBindirect", (dataset_SSB[i]))
+        indirect_SSB.append(nSSB/nGBP)
+        dose_SSB_indirect.append(dose if dose else 0)
         
-        radii_out.append(10.5+i*spacing)   
+        nDSB = calcSimpleDSBperDose("Total", (dataset_DSB[i]))
+        total_DSB.append(nDSB)
+        #dose_DSB_tot.append(dose if dose else 0)
+        nDSB = calcSimpleDSBperDose("Direct", (dataset_DSB[i]))
+        direct_DSB.append(nDSB)
+        #dose_DSB_direct.append(dose if dose else 0)
+        nDSB = calcSimpleDSBperDose("Indirect", (dataset_DSB[i]))
+        indirect_DSB.append(nDSB)
+        #dose_DSB_indirect.append(dose if dose else 0)
 
-    dose_tot = np.asarray(dose_tot)
-    dose_direct = np.asarray(dose_direct)
-    dose_indirect = np.asarray(dose_indirect)
 
-    fig1 = plt.figure(figsize=(8, 5))
-    plt.plot(radii_out, np.array(total), color='k',
+        radii_out.append(10.5+i*spacing)  
+
+
+    total_SSB = np.asarray(total_SSB)
+    direct_SSB = np.asarray(direct_SSB)
+    indirect_SSB = np.asarray(indirect_SSB)
+
+    dose_SSB_tot = np.asarray(dose_SSB_tot)
+    dose_SSB_direct = np.asarray(dose_SSB_direct)
+    dose_SSB_indirect = np.asarray(dose_SSB_indirect)
+
+    total_DSB = np.asarray(total_DSB)
+    direct_DSB = np.asarray(direct_DSB)
+    indirect_DSB = np.asarray(indirect_DSB)
+
+    dose_DSB_tot = np.asarray(dose_DSB_tot)
+    dose_DSB_direct = np.asarray(dose_DSB_direct)
+    dose_DSB_indirect = np.asarray(dose_DSB_indirect)
+
+    # not dividing by dose
+    plt.figure(figsize=(8, 5))
+    plt.plot(radii_out, total_SSB, color='k',
                 label=f'Total SB', marker='x', linewidth=.6, markersize=3)
-    plt.plot(radii_out, np.array(indirect),
+    plt.plot(radii_out, indirect_SSB,
                 color='mediumvioletred', label=f'Indirect SB',linewidth=.6, marker='o', markersize=3)
-    plt.plot(radii_out, np.array(direct), color='cornflowerblue',
+    plt.plot(radii_out, direct_SSB, color='cornflowerblue',
+            label=f'Direct SB', linewidth=.6,  marker='s', markersize=3)
+    plt.legend()
+    spacing_str = spacing if spacing else 10
+    plt.title(f"DNA damage vs distance, spacing: {spacing_str} um")
+    plt.xlabel("Radial distance (um)")
+    #plt.ylabel('Number of strand breaks $Gbp^{-1} per decay$)', fontsize=11)
+    plt.ylabel('Number of strand breaks ($Gbp^{-1}$)', fontsize=11)
+    if savefig:
+        plt.savefig(os.path.join(out_folder, f"SSB_{fname_prefix}_{spacing}um.png"))
+    else:
+        plt.show()
+
+    # Dividing by dose
+    plt.figure(figsize=(8, 5))
+    plt.plot(radii_out, total_SSB/dose_SSB_tot, color='k',
+                label=f'Total SB', marker='x', linewidth=.6, markersize=3)
+    plt.plot(radii_out, indirect_SSB/dose_SSB_indirect,
+                color='mediumvioletred', label=f'Indirect SB',linewidth=.6, marker='o', markersize=3)
+    plt.plot(radii_out, direct_SSB/dose_SSB_direct, color='cornflowerblue',
             label=f'Direct SB', linewidth=.6,  marker='s', markersize=3)
     plt.legend()
     spacing_str = spacing if spacing else 10
@@ -75,13 +140,35 @@ def plot_results(folder, fname_prefix, spacing, nevents, savefig=True, n_div_r=2
     #plt.ylabel('Number of strand breaks $Gbp^{-1} per decay$)', fontsize=11)
     plt.ylabel('Number of strand breaks ($Gy^{-1} Gbp^{-1}$)', fontsize=11)
     if savefig:
-        plt.savefig(os.path.join(out_folder, f"SSB_{fname_prefix}_{spacing}um.png"))
-    plt.show()
+        plt.savefig(os.path.join(out_folder, f"SSB_byDose_{fname_prefix}_{spacing}um.png"))
+    else:
+        plt.show()
+
+
+ # Dividing by dose
+    plt.figure(figsize=(8, 5))
+    plt.plot(radii_out, total_DSB, color='k',
+                label=f'Total DSB', marker='x', linewidth=.6, markersize=3)
+    plt.plot(radii_out, indirect_DSB,
+                color='mediumvioletred', label=f'Indirect DSB',linewidth=.6, marker='o', markersize=3)
+    plt.plot(radii_out, direct_DSB, color='cornflowerblue',
+            label=f'Direct DSB', linewidth=.6,  marker='s', markersize=3)
+    plt.legend()
+    spacing_str = spacing if spacing else 10
+    plt.title(f"DNA damage vs distance, spacing: {spacing_str} um")
+    plt.xlabel("Radial distance (um)")
+    #plt.ylabel('Number of strand breaks $Gbp^{-1} per decay$)', fontsize=11)
+    plt.ylabel('Number of Double Strand Breaks ($Gy^{-1} Gbp^{-1}$)', fontsize=11)
+    if savefig:
+        plt.savefig(os.path.join(out_folder, f"DSB_byDose_{fname_prefix}_{spacing}um.png"))
+    else:
+        plt.show()
+
 
     #DOSE
 
     fig2 = plt.figure(figsize=(8, 5))
-    plt.plot(radii_out, dose_tot, color='darkblue', marker='s', markersize=3, linestyle='-', linewidth=.9,
+    plt.plot(radii_out, dose_SSB_tot, color='darkblue', marker='s', markersize=3, linestyle='-', linewidth=.9,
                 label=f'Dose')
     plt.legend()
     # plt.xlabel('energy (MeV/$\mu$m)',fontsize=11)
@@ -92,7 +179,8 @@ def plot_results(folder, fname_prefix, spacing, nevents, savefig=True, n_div_r=2
     plt.ylabel('Dose ($Gy$)', fontsize=11)
     if savefig:
         plt.savefig(os.path.join(out_folder, f"dose_{fname_prefix}_{spacing}um.png"))
-    plt.show()
+    else:
+        plt.show()
 
     return
 
