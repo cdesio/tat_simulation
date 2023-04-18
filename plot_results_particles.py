72d2b72300905ba2d8fc82dcac17b950b0f5a0cc
Chiara De Sio <chiara.desio@live.com>
2023-04-18 19:04:55 +0100

    fixed call to plot_results

--- plot_results_particles.py
+++ plot_results_particles.py
@@ -276,11 +276,16 @@ if __name__ == "__main__":
     
     if len(spacing)>1:
         for s in spacing:
-            plot_results(folder=folder, fname_prefix=fname_prefix, 
-                         nevents=nevents, spacing=int(s), seed=seed, out_folder=out_folder, n_div_r=n_div_r, damage_type=damage_type, particle=particle)
+            #plot_results(folder=folder, fname_prefix=fname_prefix, 
+            #             nevents=nevents, spacing=int(s), seed=seed, out_folder=out_folder, n_div_r=n_div_r, damage_type=damage_type, particle=particle)
+            damage = get_results(fname_prefix = fname_prefix, nevents=n, 
+                        folder = folder, spacing=s, n_div_r=n_div_R, seed=seed,
+                        damage_type=damage_type, particle=part, keyword = keyword)
+            plot_results(damage, fname_prefix=fname_prefix, damage_type=damage_type, seed=seed)
     else:
-        plot_results(folder=folder, fname_prefix=fname_prefix, 
-                         nevents=nevents, spacing=int(spacing[0]), seed=seed, out_folder=out_folder, n_div_r=n_div_r,damage_type=damage_type, particle=particle, keyword=keyword)
-
+        damage = get_results(fname_prefix = fname_prefix, nevents=n, 
+                        folder = folder, spacing=spacing[0], n_div_r=n_div_R, seed=seed,
+                        damage_type=damage_type, particle=part, keyword = keyword)
+        plot_results(damage, fname_prefix=fname_prefix, damage_type=damage_type, seed=seed)
 
 
