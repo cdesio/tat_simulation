404e6c0ca696eb4acf14a7073c0eb1cd75c14052
Chiara De Sio <chiara.desio@live.com>
2023-04-18 19:54:01 +0100

    typo

--- plot_results_particles.py
+++ plot_results_particles.py
@@ -289,7 +289,7 @@ if __name__ == "__main__":
         # plot_results(damage, fname_prefix=fname_prefix, damage_type=damage_type, seed=seed, savefig=savefig, particle=particle)
 
         damage = {}
-        for part in ['alpha', 'e-', ' gamma']:
+        for part in ['alpha', 'e-', 'gamma']:
             damage[part] = get_results(fname_prefix = fname_prefix, nevents=nevents, 
                                     folder = folder, spacing=int(spacing[0]), n_div_r=n_div_r, seed=seed, keyword = keyword,
                                     damage_type=damage_type, particle=part)
