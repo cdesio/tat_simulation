bbc819e3165f597e0b7ee3c0cb92ade47fbc409d
Chiara De Sio <chiara.desio@bristol.ac.uk>
2023-05-19 19:05:05 +0100

    using dependencies in runscript

--- makescripts.py
+++ makescripts.py
@@ -11,19 +11,20 @@ parser.add_argument("--spacing", nargs="+",  type=float ,default = [2.0, 5.0, 10
                     help="spacing in micrometer for the dna voxels")
 
 parser.add_argument("--n_div_R", type=int,
-                    help="n of divisions in R", default = 20)
+                    help="n of divisions in R", default = 40)
 parser.add_argument("--n_div_Z", type=int,
-                    help="n of divisions in R", default = 10)
+                    help="n of divisions in Z", default = 40)
 parser.add_argument("--n_div_theta", type=int,
-                    help="n of divisions in R", default = 80)
+                    help="n of divisions in theta", default = 100)
 
 parser.add_argument("--startR", type=float, default = 10.5)
 parser.add_argument("--vessellength", type = float, default = 10.0)
-parser.add_argument("--gpsHalfZ", type = float, default = 3.0)
+parser.add_argument("--gpsHalfZ", type = float, default = 10.0)
 parser.add_argument("--n", type=int, default = 100)
 parser.add_argument("--slurm", type=bool, required=False, default = False)
 parser.add_argument("--seed", type=int, required=False)
 parser.add_argument("--nthreads", type=int, default=4)
+parser.add_argument("--mem", type=int, default=20)
 
 args = parser.parse_args()
 
@@ -43,6 +44,10 @@ if args.seed:
     seed = args.seed
 else:
     seed = random.randint(1, 10000)
+if args.mem:
+    mem = args.mem
+else:
+    mem = 20
 
 if slurm:
     simulation_parent = os.path.join(
@@ -63,8 +68,6 @@ sugarFile = os.path.join(
 time_decay = "0-10:00:00"
 time_DNA = "0-24:00:00"
 time_clustering = "0-6:00:00"
-
-mem = 10
 
 targetSize = 300  # nanometers
 gpsRadius = 10 # micrometers
@@ -111,7 +114,7 @@ dna_sim_folder = os.path.join(simulation_parent, "simulation")
 clustering_folder = os.path.join(simulation_parent, "Clustering")
 
 makerundir = lambda d: os.path.join(d, "build")
-projectcode="IFAC023282"
+projectcode="phys004501"
 
 #### create folder for current test
 test_dir = os.path.join(simulation_parent, f"output/{folder}")
@@ -180,23 +183,23 @@ for s in spacing:
     test_dir, f"run_Atdecay_{n_string}_spacing_{s_string}um_{seed}.sh")  # to be run from folder created
 
 
-    if slurm:
-        print(f"sbatch run_Atdecay_{n_string}_spacing_{s_string}um_{seed}.sh")
+    # if slurm:
+    #     print(f"sbatch run_Atdecay_{n_string}_spacing_{s_string}um_{seed}.sh")
     with open(filename_decay, "w") as f:
         f.write("#!/bin/bash --login\n")
         if slurm:
-            f.write(f"SBATCH --account={projectcode}\n")
-            f.write(f"#SBATCH --job-name=Atdecay_spacing_{s_string}um_{seed}\n")
-            f.write(f"#SBATCH --output=Atdecay_spacing_{s_string}um_{seed}.out.%J\n")
-            f.write(f"#SBATCH --error=Atdecay_spacing_{s_string}um_{seed}.err.%J\n")
+            f.write(f"#SBATCH --account={projectcode}\n")
+            f.write(f"#SBATCH --job-name=Atdecay_{n_string}_sp_{s_string}um_{seed}\n")
+            f.write(f"#SBATCH --output={test_dir}/Atdecay_spacing_{s_string}um_{seed}.out.%J\n")
+            f.write(f"#SBATCH --error={test_dir}/Atdecay_spacing_{s_string}um_{seed}.err.%J\n")
             # maximum job time in D-HH:MM
             f.write(f"#SBATCH --time={time_decay}\n")
             f.write("#SBATCH --nodes=1\n")
             f.write("#SBATCH -p short\n")
             f.write(f"#SBATCH --ntasks-per-node={numThread}\n")
-            f.write(f"#SBATCH --mem {mem}GB\n")
+            f.write(f"#SBATCH --mem 2GB\n")
             f.write("\n")
-            f.write("module load apps/geant/4.11.1.0\n")
+            f.write("module load apps/geant/4.11.0.0\n")
             f.write("module load apps/root/6.26.00\n")
 		
             f.write("source /user/home/yw18581/.bash_profile\n")
@@ -214,15 +217,15 @@ for s in spacing:
         filename_DNA = os.path.join(
         test_dir, f"run_AtDNA_{n_string}_spacing_{s_string}um_{seed}.sh"
     )  # to be run from folder created
-    if slurm:
-        print(f"sbatch run_AtDNA_{n_string}_spacing_{s_string}um_{seed}.sh")
+    # if slurm:
+    #     print(f"sbatch run_AtDNA_{n_string}_spacing_{s_string}um_{seed}.sh")
     with open(filename_DNA, "w") as f:
         f.write("#!/bin/bash --login\n")
         if slurm:
-            f.write(f"SBATCH --account={projectcode}\n")
-            f.write(f"#SBATCH --job-name=AtDNA_spacing_{s_string}um_{seed}\n")
-            f.write(f"#SBATCH --output=AtDNA_spacing_{s_string}um_{seed}.out.%J\n")
-            f.write(f"#SBATCH --error=AtDNA_spacing_{s_string}um_{seed}.err.%J\n")
+            f.write(f"#SBATCH --account={projectcode}\n")
+            f.write(f"#SBATCH --job-name=AtDNA_{n_string}_sp_{s_string}um_{seed}\n")
+            f.write(f"#SBATCH --output={test_dir}/AtDNA_spacing_{s_string}um_{seed}.out.%J\n")
+            f.write(f"#SBATCH --error={test_dir}/AtDNA_spacing_{s_string}um_{seed}.err.%J\n")
             # maximum job time in D-HH:MM
             f.write(f"#SBATCH --time={time_DNA}\n")
             f.write("#SBATCH --nodes=1\n")
@@ -230,7 +233,7 @@ for s in spacing:
             f.write(f"#SBATCH --ntasks-per-node={numThread}\n")
             f.write(f"#SBATCH --mem {mem}GB\n")
             f.write("\n")
-            f.write("module load apps/geant/4.11.1.0\n")
+            f.write("module load apps/geant/4.11.0.0\n")
             f.write("module load apps/root/6.26.00\n")
             f.write("source /user/home/yw18581/.bash_profile\n")
             f.write("source activate dart\n")
@@ -268,16 +271,16 @@ for s in spacing:
         f.write("#!/bin/bash --login\n")
 
         if slurm:
-            f.write(f"#SBATCH --job-name=clustering_At_{n_string}_spacing_{s_string}um\n")
-            f.write(f"SBATCH --account={projectcode}\n")
-            f.write(f"#SBATCH --output=clustering_At_{n_string}_spacing_{s_string}um.out.%J\n")
-            f.write(f"#SBATCH --error=clustering_At_{n_string}_spacing_{s_string}um.err.%J\n")
+            f.write(f"#SBATCH --job-name=clustering_At_{n_string}_sp_{s_string}um\n")
+            f.write(f"#SBATCH --account={projectcode}\n")
+            f.write(f"#SBATCH --output={test_dir}/clustering_At_{n_string}_spacing_{s_string}um.out.%J\n")
+            f.write(f"#SBATCH --error={test_dir}/clustering_At_{n_string}_spacing_{s_string}um.err.%J\n")
             # maximum job time in D-HH:MM
             f.write(f"#SBATCH --time={time_clustering}\n")
             f.write("#SBATCH --nodes=1\n")
             f.write("#SBATCH -p short\n")
             f.write("#SBATCH --ntasks-per-node=1\n")
-            f.write(f"#SBATCH --mem 2GB\n")
+            f.write(f"#SBATCH --mem {mem}GB\n")
             f.write("\n")
             f.write("module load apps/root/6.26.00\n")
 
@@ -306,16 +309,17 @@ for s in spacing:
 
         if slurm:
             f.write(f"#SBATCH --job-name=run_At{n_string}_sp_{s_string}um\n")
-            f.write(f"#SBATCH --output=run_At_{n_string}_spacing_{s_string}um.out.%J\n")
-            f.write(f"#SBATCH --error=run_At_{n_string}_spacing_{s_string}um.err.%J\n")
+            f.write(f"#SBATCH --account={projectcode}\n")
+            f.write(f"#SBATCH --output={test_dir}/run_At_{n_string}_spacing_{s_string}um.out.%J\n")
+            f.write(f"#SBATCH --error={test_dir}/run_At_{n_string}_spacing_{s_string}um.err.%J\n")
             f.write(f"#SBATCH --time=1-10:00\n")
             f.write("#SBATCH --nodes=1\n")
             f.write("#SBATCH -p short\n")
             f.write("#SBATCH --ntasks-per-node=1\n")
-            f.write(f"#SBATCH --mem 2GB\n")
+            f.write(f"#SBATCH --mem 1GB\n")
             f.write("\n")
             f.write("source /user/home/yw18581/.bash_profile\n")
-            f.write("module load apps/root/6.26.00\n")
+            #f.write("module load apps/root/6.26.00\n")
 
         f.write("\n")
 
@@ -326,9 +330,11 @@ for s in spacing:
             f.write(f"source {filename_clustering}\n")
             f.write(f"python {os.path.join(simulation_parent, 'plot_results_DNA.py')} --folder {clustering_outdir} --fname_prefix out_AtDNA_{n_string}_spacing --spacing {s_string} --seed {seed} --n_div_R {n_div_R} --out_folder {test_dir}")
         if  slurm:
-            f.write(f"sbatch {filename_decay}\n")
-            f.write(f"sbatch {filename_DNA}\n")
-            f.write(f"sbatch {filename_clustering}\n")
-        
-print(f"tmux new-session -d -s tat_{n_string}_{seed}\n 'source {filename_runscript}' ")
+            f.write(f"job_decay=$(sbatch --parsable {filename_decay})\n")
+            f.write(f"job_DNA=$(sbatch --parsable --dependency=afterok:$job_decay {filename_DNA})\n")
+            f.write(f"job_clustering=$(sbatch --dependency=afterany:$job_DNA {filename_clustering})\n")
+if not slurm:      
+    print(f"tmux new-session -d -s tat_{n_string}_{seed}\n 'source {filename_runscript}' ")
+else:
+    print(f"sbatch {filename_runscript}")
 print("Done.")
