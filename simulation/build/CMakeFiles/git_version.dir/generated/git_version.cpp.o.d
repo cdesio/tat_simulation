0df03b8fe187577bd1c275d78269e7f6d64e1a97
cdesio <chiara.desio@bristol.ac.uk>
2023-06-30 13:09:14 +0300

    removing build

--- simulation/build/CMakeFiles/git_version.dir/generated/git_version.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/git_version.dir/generated/git_version.cpp.o: \
- /home/cdesio/TAT/tat_simu_2steps/simulation/build/generated/git_version.cpp \
- /opt/anaconda3/envs/rootpy/x86_64-conda-linux-gnu/sysroot/usr/include/stdc-predef.h \
- /home/cdesio/TAT/tat_simu_2steps/simulation/build/generated/git_version.hh
