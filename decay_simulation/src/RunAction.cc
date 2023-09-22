671f20b7c8ffeac20b88bc8d5569b0eb5c65b673
Chiara De Sio <chiara.desio@bristol.ac.uk>
2023-09-22 10:31:26 +0100

    adding time to tracking

--- decay_simulation/src/RunAction.cc
+++ decay_simulation/src/RunAction.cc
@@ -109,6 +109,7 @@ void RunAction::CreateNtuple()
   analysisManager->CreateNtupleIColumn(0, "parentID");
   analysisManager->CreateNtupleDColumn(0, "particleEnergy");
   analysisManager->CreateNtupleDColumn(0, "stepLength");
+  analysisManager->CreateNtupleDColumn(0, "time");
 
   analysisManager->FinishNtuple(0);
 
