5263255a7ba2a8bccf9e66f0dbca3275fe2f12af
cdesio <chiara.desio@bristol.ac.uk>
2023-11-03 20:02:47 +0300

    changed histone to ellipsoid, forced distances to nm

--- simulation/src/DetectorConstruction.cc
+++ simulation/src/DetectorConstruction.cc
@@ -116,6 +116,7 @@ G4VPhysicalVolume *DetectorConstruction::Construct()
 
   G4NistManager *man = G4NistManager::Instance();
   G4Material *waterMaterial = man->FindOrBuildMaterial("G4_WATER");
+  G4Material *vacuum = man->FindOrBuildMaterial("G4_Galactic");
   G4Material *air = man->FindOrBuildMaterial("G4_AIR");
 
   G4Box *solidWorld{nullptr};
@@ -198,12 +199,8 @@ G4VPhysicalVolume *DetectorConstruction::Construct()
                                                      waterMaterial,
                                                      "sugar1");
 
-  G4Tubs *solidHistone = new G4Tubs("histone",
-                                    0,
-                                    2.9 * nanometer,
-                                    2.75 * nanometer,
-                                    0 * degree,
-                                    360 * degree);
+  G4Ellipsoid *solidHistone = new G4Ellipsoid("histone", 2.5 * nanometer, 2.5 * nanometer, 2.5 * nanometer);
+
 
   G4LogicalVolume *logicHistone = new G4LogicalVolume(solidHistone,
                                                       waterMaterial,
@@ -211,37 +208,43 @@ G4VPhysicalVolume *DetectorConstruction::Construct()
 
   Command *command = CommandLineParser::GetParser()->GetCommandIfActive("-sugar");
 
-  ifstream f(command->GetOption(), ios::in); // open file
+  ifstream f;
+  f.open(command->GetOption()); 
 
-  if (!f.is_open())
+  if (f.is_open())
+  {
+    G4cout << "Sugar file opened OK: " << command->GetOption() << G4endl;
+  }
+  else
   {
     throw std::runtime_error("*******SUGAR GEOMETRY FILE NOT FOUND OR OPENED CORRECTY********");
   }
 
   G4int copynum{0};
-  while (!f.eof())
+  float line[12];
+
+  while (f.read((char *)&line, sizeof line))
   {
-    double x1, y1, z1, x2, y2, z2, base_x1, base_y1, base_z1, base_x2, base_y2, base_z2;
-    f >> x1 >> y1 >> z1 >> x2 >> y2 >> z2 >> base_x1 >> base_y1 >> base_z1 >> base_x2 >> base_y2 >> base_z2;
-    fPositions0->Insert(G4ThreeVector(x1,
-                                      y1,
-                                      z1));
-    fPositions1->Insert(G4ThreeVector(x2,
-                                      y2,
-                                      z2));
-    fPositionsBase0->Insert(G4ThreeVector(base_x1,
-                                          base_y1,
-                                          base_z1));
-    fPositionsBase1->Insert(G4ThreeVector(base_x2,
-                                          base_y2,
-                                          base_z2));
+    float x1 = line[0], y1 = line[1], z1 = line[2], x2 = line[3], y2 = line[4], z2 = line[5], base_x1 = line[6], base_y1 = line[7], base_z1 = line[8], base_x2 = line[9], base_y2 = line[10], base_z2 = line[11];
+    fPositions0->Insert(G4ThreeVector(x1*nm,
+                                      y1*nm,
+                                      z1*nm));
+    fPositions1->Insert(G4ThreeVector(x2*nm,
+                                      y2*nm,
+                                      z2*nm));
+    fPositionsBase0->Insert(G4ThreeVector(base_x1*nm,
+                                          base_y1*nm,
+                                          base_z1*nm));
+    fPositionsBase1->Insert(G4ThreeVector(base_x2*nm,
+                                          base_y2*nm,
+                                          base_z2*nm));
     if (placeSugars)
     {
 
       new G4PVPlacement(0,
-                        G4ThreeVector(x1,
-                                      y1,
-                                      z1),
+                        G4ThreeVector(x1*nm,
+                                      y1*nm,
+                                      z1*nm),
                         logicSugar0,
                         "sugar0",
                         logicChromatinSegment,
@@ -250,9 +253,9 @@ G4VPhysicalVolume *DetectorConstruction::Construct()
                         false);
 
       new G4PVPlacement(0,
-                        G4ThreeVector(x2,
-                                      y2,
-                                      z2),
+                        G4ThreeVector(x2*nm,
+                                      y2*nm,
+                                      z2*nm),
                         logicSugar1,
                         "sugar1",
                         logicChromatinSegment,
@@ -266,20 +269,27 @@ G4VPhysicalVolume *DetectorConstruction::Construct()
   // Histones
   command = CommandLineParser::GetParser()->GetCommandIfActive("-histone");
 
-  ifstream f2(command->GetOption(), ios::in);
-  if (!f2.is_open())
+  ifstream f2;
+  f2.open(command->GetOption()); 
+
+  if (f2.is_open())
+  {
+    G4cout << "Histone file opened OK: " << command->GetOption() << G4endl;
+  }
+  else
   {
     throw std::runtime_error("*******HISTONE GEOMETRY FILE NOT FOUND OR OPENED CORRECTY********");
   }
 
-  while (!f2.eof())
+  float line2[6];
 
+  while (f2.read((char *)&line2, sizeof line2))
   {
-    double x1, y1, z1, rotx, roty, rotz;
-    f2 >> x1 >> y1 >> z1 >> rotx >> roty >> rotz;
-    fPositions.push_back(G4ThreeVector(x1,
-                                       y1,
-                                       z1));
+    float x1 = line2[0], y1 = line2[1], z1 = line2[2], rotx = line2[3], roty = line2[4], rotz = line2[5];
+
+    fPositions.push_back(G4ThreeVector(x1*nm,
+                                       y1*nm,
+                                       z1*nm));
     fRotations.push_back(new G4RotationMatrix(roty, // geant convention is y,z,x
                                               rotz,
                                               rotx));
