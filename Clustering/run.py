9a4d53b2b41f34536c6262a5c2227318c6d3a323
cdesio <chiara.desio@bristol.ac.uk>
2023-11-13 16:16:13 +0300

    adapt to mine

--- Clustering/run.py
+++ Clustering/run.py
@@ -4,27 +4,22 @@
 import argparse
 
 # currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-# sys.path.insert(0, parentdir)
+# sys.path.insert(0, parentdir) 
 
 import runClustering
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--filename", type=str,
-                    help="Root file to run clustering on")
-parser.add_argument("--filenamePhoton", type=str,
-                    help="Root file to run clustering on")
+parser.add_argument("--filename", type=str, help="Root file to run clustering on")
+parser.add_argument("--filenamePhoton", type=str, help="Root file to run clustering on")
 parser.add_argument("--output", type=str, help="outfile name")
-parser.add_argument("--min", type=float,
-                    help="Emin direct damage, default 5eV")
-parser.add_argument("--max", type=float,
-                    help="Emax direct damage, default 37.5eV")
-parser.add_argument("--indirect", type=float,
-                    help="Indirect damage percentage, default 0.405")
+parser.add_argument("--min", type=float, help="Emin direct damage, default 5eV")
+parser.add_argument("--max", type=float, help="Emax direct damage, default 37.5eV")
+parser.add_argument("--indirect", type=float, help="Indirect damage percentage, default 0.405")
 parser.add_argument("--sugar", type=str, help="sugar geometry file")
-parser.add_argument("--sepR", type=bool, help="save to separate files for R")
-parser.add_argument("--n_boxes", type=int, help="tot number of dna boxes", default=3200)
-parser.add_argument("--boxes_per_R", type=int, help="number of boxes per R division", default=800)
-
+parser.add_argument("--continuous", type=bool, help="is geometry continuous")
+parser.add_argument("--copy", type=int, help="copy number to run clustering in")
+parser.add_argument("--simulationType", type=str, help="simulationType: standalone, decay, photon")
+parser.add_argument("--part1_particleSource", type=str, help="particle: alpha, e-")
 
 
 args = parser.parse_args()
@@ -61,18 +56,28 @@ else:
 
 if args.sugar:
     sugarPosFilename = args.sugar
-if args.sepR:
-    separate_r = args.sepR
-
-if args.n_boxes:
-    n_boxes = args.n_boxes
-if args.boxes_per_R:
-    boxes_per_R = args.boxes_per_R
-    
 else:
     print("Missing sugar geometry file")
 
+if args.continuous:
+    continuous = args.continuous
+else:
+    continuous = 1
 
-runClustering.runClustering(filename, outputFilename, fEMinDamage, fEMaxDamage,
-                            probIndirect, sugarPosFilename, filenamePhoton=filenamePhoton, separate_r=separate_r, 
-                            n_boxes = n_boxes, boxes_per_R = boxes_per_R)
+if args.copy:
+    copy = int(args.copy)
+else:
+    copy = 0
+
+if args.simulationType:
+    simulationType = args.simulationType
+else:
+    simulationType = "standalone"
+
+if args.part1_particleSource:
+    part1_particleSource = args.part1_particleSource.strip("[]").split(",")
+else:
+    part1_particleSource = False
+
+runClustering.runClustering(filename, outputFilename, fEMinDamage, fEMaxDamage, probIndirect, sugarPosFilename, filenamePhoton=filenamePhoton, continuous =continuous, part1_CopyNum = copy, simulationType = simulationType, part1_particleSource = part1_particleSource)
+
