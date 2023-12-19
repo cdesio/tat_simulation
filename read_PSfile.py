101c08a3ecbe2441163b2a899bb0ca7aa41a107b
cdesio <chiara.desio@bristol.ac.uk>
2023-12-19 14:43:33 +0300

    update particle map +  added global positions

--- read_PSfile.py
+++ read_PSfile.py
@@ -1,15 +1,23 @@
 import numpy as np
 
-
-particle_map = {1: "alpha",
-                2: "gamma",
-                3: "e-",
-                4: "nu_e",
-                5: "At211",
-                6: "Po211",
-                7: "Bi207",
-                8: "Pb207",
-                9: "e+" }
+particle_map = {0: "At211",
+      1:  "Po211",
+      2: "Po211*", 
+      3: "Bi207",
+      4: "Pb207",
+      5: "Pb207*",
+      6:"alphaAt211",
+      7: "alphaPo211",
+      8: "e-At211",
+      9: "e-Bi207",
+      10: "e-Pb207*",
+      11: "gammaAt211",
+      12: "gammaBi207",
+      13: "gammaPb207",
+      14: "gammaPb207*",
+      15: "gammaPo211",
+      16: "gammaPo211*",
+   -1: "all"}
 
 
 from dataclasses import dataclass, InitVar, field
@@ -18,7 +26,8 @@ from typing import List
 @dataclass
 class Event:
     psdata: InitVar[List[float]]
-    positions: List[float] = field(init=False)
+    localpositions: List[float] = field(init=False)
+    globalpositions: List[float] = field(init=False)
     momentum: List[float] = field(init=False)
     energy: float = field(init=False)
     eventID: int = field(init=False)
@@ -30,33 +39,38 @@ class Event:
 
     def __post_init__(self, data_arr):
             if data_arr is not None:
-                self.positions = data_arr[:3]
-                self.momentum = data_arr[3:6]
-                self.energy = data_arr[6]
-                self.eventID = int(data_arr[7])
-                self.particleID = int(data_arr[8])
-                self.copyNo = int(data_arr[9])
-                self.time = data_arr[10]
-                self.parent = data_arr[11]
-                # if len(data_arr)>11:
-                #     self.excitation = data_arr[12]
+                if len(data_arr)==12:
+                    self.localpositions = data_arr[:3]
+                    self.momentum = data_arr[3:6]
+                    self.energy = data_arr[6]
+                    self.eventID = int(data_arr[7])
+                    self.particleID = int(data_arr[8])
+                    self.copyNo = int(data_arr[9])
+                    self.time = data_arr[10]
+                    self.parent = data_arr[11]
+                elif len(data_arr)==15:
+                    self.globalpositions = data_arr[:3]
+                    self.localpositions = data_arr[3:6]
+                    self.momentum = data_arr[6:9]
+                    self.energy = data_arr[9]
+                    self.eventID = int(data_arr[10])
+                    self.particleID = int(data_arr[11])
+                    self.copyNo = int(data_arr[12])
+                    self.time = data_arr[13]
+                    self.parent = data_arr[14]
 
 
 class PSEvents:
     def __init__(self, psfile):
         self.events=[]
         self.psfile = psfile
-        events_data = np.fromfile(self.psfile, dtype='float32').reshape(-1,12)
+        events_data = np.fromfile(self.psfile, dtype='float32').reshape(-1,15)
         for evt in events_data:
             self.events.append(Event(evt))
     @property
     def energies(self):
         if len(self.events):
             return np.array([evt.energy for evt in self.events])
-    # @property
-    # def exc_energies(self):
-    #     if len(self.events):
-    #         return np.array([evt.excitation for evt in self.events])
     @property
     def eventIDs(self):
         if len(self.events):
